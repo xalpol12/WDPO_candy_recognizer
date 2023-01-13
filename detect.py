4dbcd3f8d0665cb719ca518ef1c1d5715f5cccbf
Dawid Stawiszynski <dawid.stawiszynski@student.put.poznan.pl>
2023-01-13 12:02:08 +0100

    Fix detect() return value

--- detect.py
+++ detect.py
@@ -7,13 +7,39 @@ import cv2
 import numpy as np
 from tqdm import tqdm
 
-# #HSV in range param values for each color:
-# hsv_config_low = np.array([[114, 128, 78], [87, 220, 132],
-#                            [0, 134, 43], [123, 77, 38]])
-# hsv_config_high = np.array([[125, 239, 246], [115, 255, 190],
-#                             [95, 255, 93], [180, 255, 117]])
-# areas = np.array([[0.0001], [0.00004],
-#                   [0.00006], [0.00005]])
+
+hsv_config_low = np.array([[114, 128, 78], [87, 220, 132],
+                           [0, 134, 43], [123, 77, 38]])
+hsv_config_high = np.array([[125, 239, 246], [115, 255, 190],
+                            [95, 255, 93], [180, 255, 117]])
+areas = np.array([[0.0001], [0.00004],
+                  [0.00006], [0.00005]])
+
+def process_image(image, low_HSV, high_HSV):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    filtered = cv2.bilateralFilter(gray, 5, 20, 10)
+    threshold = cv2.inRange(filtered, low_HSV, high_HSV)
+    return threshold
+
+def create_mask(mask):
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    mask_close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask_open = cv2.morphologyEx(mask_close, cv2.MORPH_OPEN, kernel)
+    return mask_open
+
+def get_contours(mask_open):
+    contours, _ = cv2.findContours(mask_open, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours
+
+def get_candy_count(contours, image, area):
+    height = 0;
+    width = 0
+    height, width = image.shape[:2]
+    candy_count = 0
+    for contour in contours:
+        if cv2.contourArea(contour) > area * height * width:
+            candy_count += 1
+    return candy_count
 
 
 def detect(img_path: str) -> Dict[str, int]:
@@ -29,40 +55,6 @@ def detect(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    # #HSV in range param values for each color:
-    hsv_config_low = np.array([[114, 128, 78], [87, 220, 132],
-                               [0, 134, 43], [123, 77, 38]])
-    hsv_config_high = np.array([[125, 239, 246], [115, 255, 190],
-                                [95, 255, 93], [180, 255, 117]])
-    areas = np.array([[0.0001], [0.00004],
-                      [0.00006], [0.00005]])
-
-    def process_image(image, low_HSV, high_HSV):
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-        filtered = cv2.bilateralFilter(gray, 5, 20, 10)
-        threshold = cv2.inRange(filtered, low_HSV, high_HSV)
-        return threshold
-
-    def create_mask(mask):
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-        mask_close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-        mask_open = cv2.morphologyEx(mask_close, cv2.MORPH_OPEN, kernel)
-        return mask_open
-
-    def get_contours(mask_open):
-        contours, _ = cv2.findContours(mask_open, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        return contours
-
-    def get_candy_count(contours, image, area):
-        height = 0;
-        width = 0
-        height, width = image.shape[:2]
-        candy_count = 0
-        for contour in contours:
-            if cv2.contourArea(contour) > area * height * width:
-                candy_count += 1
-        return candy_count
-
 
     #TODO: Implement detection method.
     candies = [0, 0, 0, 0]
@@ -73,7 +65,7 @@ def detect(img_path: str) -> Dict[str, int]:
         contours = get_contours(mask)
         count = get_candy_count(contours, image, areas[i])
         candies[i] = count
-        return {'red': candies[0], 'yellow': candies[1], 'green': candies[2], 'purple': candies[3]}
+    return {'red': candies[0], 'yellow': candies[1], 'green': candies[2], 'purple': candies[3]}
 
 
 @click.command()
